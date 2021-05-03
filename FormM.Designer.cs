92f1beed050d96cf316ef103228cde00b59d4b38
Евгений Фатеев <atd6@mail.ru>
2021-05-03 19:34:15 +0700

    First Commit

---
+++ FormM.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace GenAlg
+{
+    partial class FormM
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 84);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(17, 5);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(271, 164);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(155, 61);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(133, 65);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "до";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(77, 61);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 65);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Отрезок от";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Точность решения";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 18);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 111);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Время выполнения:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 139);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Наилучшая хромосома:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Кол-во хромосом";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Количество поколений:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 173);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(271, 251);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // FormM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 433);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormM";
+            this.Text = "Генетический алгоритм";
+            this.Load += new System.EventHandler(this.FormM_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FormM.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenAlg
+{
+    public partial class FormM : Form
+    {
+        public FormM()
+        {
+            InitializeComponent();
+        }
+
+        List<double> population = new List<double>();
+        List<double> Func = new List<double>();
+        int pop_size;
+        Random rand = new Random();
+
+        double Function(double x) //вычисление функции - второй производной
+        {
+            return 6.75 * Math.Pow(0.6667 * x - 1, 2.0);
+        }
+
+        double Mutaciya(double x0, double x1)  // мутация: генерация случайной величины
+        {
+            const int N = 100000000;            
+            return Math.Abs((double)(rand.Next(N) * (x1 - x0) / N)) + x0;
+        }
+
+        double Inversiya(double x, double ee)  // инверсия: поиск в окрестностях точки
+        {
+            int znak = 0;
+            znak++;
+            znak %= 2;
+            if (znak == 0) 
+                return x - ee;
+            else 
+                return x + ee;
+        }
+
+        void Crossover(List<double> x, double eps, double x0, double x1)  
+            // кроссовер: вычисляем среднее арифметическое между двумя
+        {
+            int k = pop_size-1;
+            for (int i = 0; i < pop_size/5; i++)
+                for (int j = i + 1; j < pop_size/5; j++)
+                {
+                    if (k < 0) break;
+                    x[k] = (x[i] + x[j]) / 2;
+                    k--;
+                }
+            for (int i = 0; i < pop_size/5; i++)
+            {
+                if (k < 0) break;
+                x[k] = Inversiya(x[i], eps); 
+                k--;
+                if (k < 0) break;
+                x[k] = Inversiya(x[i], eps); 
+                k--;
+            }
+            for (int i = pop_size/5; i < k; i++)
+                x[i] = Mutaciya(x0, x1);
+        }
+
+        void Sort(List<double> x, List<double> y)  // сортировка
+        {
+            for (int i = 0; i < pop_size; i++)
+                for (int j = i + 1; j < pop_size; j++)
+                    if (Math.Abs(y[j]) < Math.Abs(y[i]))
+                    {
+                        double help = y[i];
+                        y[i] = y[j];
+                        y[j] = help;
+                        help = x[i];
+                        x[i] = x[j];
+                        x[j] = help;
+                    }
+        }
+
+        double GeneticAlg(double x0, double x1, double eps)  // поиск решения с использованием ГА
+        {
+            int iter = 0;
+            richTextBox1.Text += "Формирование начальной популяции\n";
+            string s = "[";
+            for (int i = 0; i < pop_size; i++)   // Формирование начальной популяции
+            {
+                population.Add(Mutaciya(x0, x1));
+                Func.Add(Function(population[i]));
+                s += this.population[i] + ", ";                
+            }
+            s = s.Substring(0, s.Length - 2);
+            s += "]\n";
+            richTextBox1.Text += s;            
+
+            Sort(population, Func);
+
+            do
+            {
+                iter++;
+                richTextBox1.Text += "\n=== " + iter.ToString() + " поколение ===\n";
+                s = "[";
+                Crossover(population, eps, x0, x1); //выполнение процедуры кроссовера
+                for (int i = 0; i < pop_size; i++)
+                {
+                    Func[i] = Function(population[i]);
+                    s += this.population[i] + ", ";
+                }
+                s = s.Substring(0, s.Length - 2);
+                s += "]\n";
+                richTextBox1.Text += s;
+
+                Sort(population, Func);
+            }
+            while (Math.Abs(Func[0]) > eps && iter < 20000);
+            richTextBox1.Text += "\n=== " + iter.ToString() + " поколение ===\n";
+            label3.Text = "Количество поколений:" + iter.ToString();
+            return population[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pop_size = int.Parse(textBox2.Text); //размерность популяции              
+            DateTime dt = DateTime.Now;
+            //Выполняем генерирование особей            
+            label4.Text = "Наилучшая хромосома:" + GeneticAlg(float.Parse(textBox3.Text), 
+                float.Parse(textBox4.Text), float.Parse(textBox1.Text)).ToString();
+            richTextBox1.Text += "Время работы программы:" + (DateTime.Now - dt).ToString();
+            label5.Text = "Время работы программы:" + (DateTime.Now - dt).ToString();
+        }
+
+        private void FormM_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
